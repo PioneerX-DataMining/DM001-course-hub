9c9e9f24017d0538c4448d4fd1aae4eb0fb6b63f
smallporcellus <wchgeng@buct.edu.cn>
2026-09-15 17:33:42 +0800

    Bust shared deck assets after observer loop fix

--- concepts/deck-mobile.js
+++ concepts/deck-mobile.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION = '20260915-1745';
+  const VERSION = '20260915-1800';
   const self = document.currentScript && document.currentScript.src;
   const base = self ? new URL('.', self).href : new URL('./', location.href).href;
   const esc = (url) => String(url).replace(/&/g, '&amp;').replace(/"/g, '&quot;');
