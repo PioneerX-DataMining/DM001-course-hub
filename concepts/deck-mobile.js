43aa6d288bc085100828ebe61a2fd973c829e747
smallporcellus <wchgeng@buct.edu.cn>
2026-09-15 16:09:37 +0800

    Load shared concept deck standards on every concept page

--- concepts/deck-mobile.js
+++ concepts/deck-mobile.js
@@ -3,6 +3,7 @@
   const base = self ? new URL('.', self).href : new URL('./', location.href).href;
   const esc = (url) => String(url).replace(/&/g, '&amp;').replace(/"/g, '&quot;');
   document.write(
+    `<link rel="stylesheet" href="${esc(new URL('concept-standards.css', base).href)}">` +
     `<script src="${esc(new URL('deck-mobile-core.js', base).href)}"><\/script>` +
     `<script src="${esc(new URL('deck-editor.js', base).href)}"><\/script>`
   );
